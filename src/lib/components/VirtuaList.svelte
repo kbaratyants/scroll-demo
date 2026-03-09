3fbc7e984065311fa56dd1a17cbccb698fd11a12
Karen Baratyants <kbaratyants@gmail.com>
2026-03-09 16:37:02 +0300

    task 5 add virtua

---
+++ src/lib/components/VirtuaList.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { VList } from "virtua/svelte";
+	import MessageItem from "$lib/components/MessageItem.svelte";
+	import type { Message } from "$lib/data/messages";
+
+	let { messages }: { messages: Message[] } = $props();
+</script>
+
+<VList data={messages} style="height: 600px;" getKey={(message) => message.id}>
+	{#snippet children(message)}
+		<MessageItem {message} />
+	{/snippet}
+</VList>
