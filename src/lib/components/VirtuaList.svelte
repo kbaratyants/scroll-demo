cfce2834131401133c58e1ef63292705e9217ce3
Karen Baratyants <kbaratyants@gmail.com>
2026-03-09 16:53:07 +0300

    task 6

--- src/lib/components/VirtuaList.svelte
+++ src/lib/components/VirtuaList.svelte
@@ -3,10 +3,10 @@
 	import MessageItem from "$lib/components/MessageItem.svelte";
 	import type { Message } from "$lib/data/messages";
 
-	let { messages }: { messages: Message[] } = $props();
+	let { items }: { items: Message[] } = $props();
 </script>
 
-<VList data={messages} style="height: 600px;" getKey={(message) => message.id}>
+<VList data={items} style="height: 600px;" getKey={(message) => message.id}>
 	{#snippet children(message)}
 		<MessageItem {message} />
 	{/snippet}
